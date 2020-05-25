6979742c9a59a06be167564dfaf050d4338737ab
German Costa Gimenez <gcg30@alu.ua.es>
2020-05-26 01:29:26 +0200

    añadido servicio demandante(isn terminar)

---
+++ servidores/ServiciosRest/Contracts/IDemandanteController.cs
@@ -0,0 +1,22 @@
+// Template: Controller Interface (ApiControllerInterface.t4) version 5.0
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ServiciosRest.Demandantes.Models;
+
+
+namespace ServiciosRest.Demandantes
+{
+    public interface IDemandanteController
+    {
+
+        IHttpActionResult Post([FromBody] ServiciosRest.Demandantes.Models.Demandante demandante);
+        IHttpActionResult Get([FromUri] string Dni);
+        IHttpActionResult GetByDniSituacionLaboral([FromUri] string Dni);
+        IHttpActionResult Put([FromBody] string Ipdefault,[FromUri] string Dni);
+        IHttpActionResult PutByDniFechaRenovacion([FromBody] string Ipdefault,[FromUri] string Dni,[FromUri] string restkey);
+    }
+}
---
+++ servidores/ServiciosRest/Controllers/DemandanteController.cs
@@ -0,0 +1,196 @@
+// Template: Controller Implementation (ApiControllerImplementation.t4) version 5.0
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using MySql.Data.MySqlClient;
+using ServiciosRest.Demandantes.Models;
+
+namespace ServiciosRest.Demandantes
+{
+    public partial class DemandanteController : IDemandanteController
+    {
+
+/// <summary>
+		/// Dará de alta un nuevo demandante en el sistema. - /demandante
+		/// </summary>
+		/// <param name="demandante">Tipo de dato complejo para representar los datos de un demandante</param>
+		/// <returns>MultipleDemandantePost</returns>
+        public IHttpActionResult Post([FromBody] ServiciosRest.Demandantes.Models.Demandante demandante)
+        {
+            // TODO: implement Post - route: demandante/demandante
+			// var result = new MultipleDemandantePost();
+			// return Ok(result);
+			return Ok();
+        }
+
+/// <summary>
+		/// obtiene el perfil de un demandante - /demandante/{dni}
+		/// </summary>
+		/// <param name="Dni"></param>
+		/// <returns>MultipleDemandanteDniGet</returns>
+        public IHttpActionResult Get([FromUri] string Dni)
+        {
+            // TODO: implement Get - route: demandante/{id}
+            // var result = new MultipleDemandanteIdGet();
+            // return Ok(result);
+            MultipleDemandanteDniGet resp = new MultipleDemandanteDniGet();
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection("host=localhost; port=3306; user=usuario; password=; database=mtis_final");
+                connection.Open();
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM demandante WHERE dni=@dni";
+                command.Prepare();
+                command.Parameters.AddWithValue("@dni", Dni);
+                MySqlDataReader resultSet = command.ExecuteReader();
+                Demandante demandante = new Demandante();
+
+                if (resultSet.Read())
+                {
+                    demandante.Dni = resultSet[1].ToString();
+                    demandante.Pass = resultSet[2].ToString();
+                    demandante.Nombre = resultSet[3].ToString();
+                    demandante.Apellidos = resultSet[4].ToString();
+                    demandante.Direccion = resultSet[5].ToString();
+                    demandante.Poblacion = resultSet[6].ToString();
+                    demandante.Telefono = resultSet[7].ToString();
+                    demandante.Email = resultSet[8].ToString();
+                    demandante.Fecha_nacimiento = resultSet[9].ToString();
+                    demandante.Fecha_renovacion = resultSet[10].ToString();
+                    demandante.Iban = resultSet[11].ToString();
+                    demandante.Situacion_laboral = (bool)resultSet[12];
+                    demandante.Edad = (int)resultSet[13];
+                    demandante.Titulos= resultSet[14].ToString();
+                    demandante.Experiencia= (int)resultSet[15];
+
+
+                    resp.Demandante = demandante;
+                    resultSet.Close();
+                    return Ok(resp.Demandante);
+                }
+
+                else
+                {
+                    resp.Error = new Error();
+                    resp.Error.Codigo = 404;
+                    resp.Error.Mensaje = "No se encuentra el demandante con ese DNI";
+                    return Content(System.Net.HttpStatusCode.NotFound, resp.Error);
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                return InternalServerError();//   Content(System.Net.HttpStatusCode.InternalServerError);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+               
+            }
+            
+        }
+
+        
+        /// <summary>
+        /// Devuelve true si la situacion laboral es valida para solicitar un empleo. - /demandante/{dni}/situacionLaboral
+        /// </summary>
+        /// <param name="Dni"></param>
+        /// <returns>MultipleDemandanteDniSituacionLaboralGet</returns>
+        public IHttpActionResult GetByDniSituacionLaboral([FromUri] string Dni)
+        {
+            // TODO: implement GetByDniSituacionLaboral - route: demandante/{dni}/situacionLaboral
+            // var result = new MultipleDemandanteDniSituacionLaboralGet();
+            // return Ok(result);
+            MultipleDemandanteDniSituacionLaboralGet resp = new MultipleDemandanteDniSituacionLaboralGet();
+            MySqlConnection connection = null;
+
+            try
+            {
+                connection = new MySqlConnection("host=localhost; port=3306; user=usuario; password=; database=mtis_final");
+                connection.Open();
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM demandante WHERE dni=@dni";
+                command.Prepare();
+                command.Parameters.AddWithValue("@dni", Dni);
+                MySqlDataReader resultSet = command.ExecuteReader();
+                Response response = new Response();
+
+                if (resultSet.Read())
+                {
+                    response.Estado = (bool)resultSet[12];
+                    if (response.Estado)//1 = demandante empleado actualmente
+                    {
+                        response.Mensaje = "Trabajando";
+                    }
+                    else{//0 para desempleado
+                        response.Mensaje = "Desempleado";
+                    }
+
+
+                    resp.Response = response;
+                    resultSet.Close();
+                    return Ok(resp.Response);
+                }
+
+                else
+                {
+                    resp.Error = new Error();
+                    resp.Error.Codigo = 404;
+                    resp.Error.Mensaje = "No se encuentra el demandante con ese DNI";
+                    return Content(System.Net.HttpStatusCode.NotFound, resp.Error);
+                }
+
+            }
+
+            catch (Exception e)
+            {
+                return InternalServerError();//   Content(System.Net.HttpStatusCode.InternalServerError);
+            }
+            finally
+            {
+                if (connection != null)
+                    connection.Close();
+
+            }
+        }
+
+/// <summary>
+		/// Modificará la contraseña del demandante. Será útil tanto en el alta de un nuevo usuario como en el caso en que no recuerde su contraseña. - /demandante/{dni}/pass
+		/// </summary>
+		/// <param name="Ipdefault"></param>
+		/// <param name="Dni"></param>
+		/// <returns>MultipleDemandanteDniPassPut</returns>
+        public IHttpActionResult Put([FromBody] string Ipdefault,[FromUri] string Dni)
+        {
+            // TODO: implement Put - route: demandante/{dni}/pass
+			// var result = new MultipleDemandanteDniPassPut();
+			// return Ok(result);
+			return Ok();
+        }
+
+/// <summary>
+		/// Modificará la fecha de renovación del demandante. Servirá para cuando el demandante decida renovar su estancia en el sistema. - /demandante/{dni}/fechaRenovacion
+		/// </summary>
+		/// <param name="Ipdefault"></param>
+		/// <param name="Dni"></param>
+		/// <param name="restkey"></param>
+		/// <returns>MultipleDemandanteDniFechaRenovacionPut</returns>
+        public IHttpActionResult PutByDniFechaRenovacion([FromBody] string Ipdefault,[FromUri] string Dni,[FromUri] string restkey)
+        {
+            // TODO: implement PutByDniFechaRenovacion - route: demandante/{dni}/fechaRenovacion
+			// var result = new MultipleDemandanteDniFechaRenovacionPut();
+			// return Ok(result);
+			return Ok();
+        }
+
+    }
+}
